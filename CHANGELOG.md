7f1ecaa3d301ef5a71aceea9baa2f0d947f45308
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-12 16:46:45 -0700

    Update for release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+#v0.1.0
+* Fix require
+
+# v0.0.1
+* Initial release
--- lib/knife-remotelxc/version.rb
+++ lib/knife-remotelxc/version.rb
@@ -1,5 +1,5 @@
 module KnifeRemoteLXC
   class Version < Gem::Version
   end
-  VERSION = Version.new('0.0.1')
+  VERSION = Version.new('0.1.0')
 end
